b51d0df6faaa96de58a8e3bebc04dbb95a8cb024
joyhillgames <144593357+joyhillgames@users.noreply.github.com>
2025-05-05 20:21:09 +0300

    Added buttons for adding and deleting cards

--- src/components/KanbanBoard.jsx
+++ src/components/KanbanBoard.jsx
@@ -1,16 +1,42 @@
 import KanbanCard from "./KanbanCard"
 import "../index.css"
+import { useMemo, useState } from "react"
+import { DndContext } from "@dnd-kit/core"
+import { SortableContext } from "@dnd-kit/sortable"
 
 function KanbanBoard(props) {
-    const { kanbanCards, kanbanCardItems } = props
+    const [columns, setColumns] = useState([])
+    const columnsId = useMemo(() => columns.map((col) => col.id), [columns])
+
+    function createNewColumn() {
+        const columnToAdd = {
+            id: generateId(),
+            title: `Column ${columns.length + 1}`
+        }
+        setColumns([...columns, columnToAdd])
+    }
+
+    function generateId() {
+        return Math.floor(Math.random() * 10001)
+    }
+
+    function deleteColumn(id) {
+        const filteredColumns = columns.filter((col) => col.id !== id)
+        setColumns(filteredColumns)
+    }
 
     return (
-        <div className="kanban-board">
-            {kanbanCards.map((kanbanCard, cardIndex) => {
-                return (
-                    <KanbanCard kanbanCard={kanbanCard} kanbanCardItems={kanbanCardItems} key={cardIndex}/>
-                )
-            })}
+        <div>
+            <DndContext>
+                <div className="kanban-board">
+                    <SortableContext items={columnsId}>
+                        {columns.map((column, columnIndex) => (
+                            <KanbanCard className="kanban-card" column={column} deleteColumn={deleteColumn} key={column.id}/>
+                        ))}
+                    </SortableContext>
+                    <button className="add-column-btn" onClick={() => {createNewColumn()}}>Add Column</button>
+                </div>
+            </DndContext>
         </div>
     )
 }
--- src/components/KanbanCard.jsx
+++ src/components/KanbanCard.jsx
@@ -1,19 +1,33 @@
+import { useSortable } from "@dnd-kit/sortable"
 import "../index.css"
+import { CSS } from "@dnd-kit/utilities"
 import KanbanCardItem from "./KanbanCardItem"
 
 function KanbanCard(props) {
-    const { kanbanCard, kanbanCardItems } = props
+    const { column, deleteColumn } = props
+
+    const { setNodeRef, attributes, listeners, transform, transition } = useSortable({
+        id: column.id,
+        data: {
+            type: "Column",
+            column,
+        },
+    })
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform),
+    }
 
     return (
         <div className="kanban-card">
-            <div className="kanban-card-header">{kanbanCard.name}</div>
-            <div className="kanban-card-content">
-                {kanbanCardItems.filter((i) => i.parentIndex === kanbanCard.index).map((kanbanCardItem, kanbanCardItemIndex) => {
-                    return(
-                        <KanbanCardItem kanbanCardItem={kanbanCardItem} key={kanbanCardItemIndex}/>
-                    )
-                })}
+            <div className="kanban-card-header">
+                <div>0</div>
+                <div>{column.title}</div>
+                <button onClick={() => {deleteColumn(column.id)}}><i class="fa-solid fa-trash-can"></i></button>
             </div>
+            <div className="kanban-card-content">Content</div>
+            <div>Footer</div>
         </div>
     )
 }
